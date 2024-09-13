6620529f4bc1abf3258bb3953586f6e163aeb5d2
mocaaaaaaa <jeawon1337@gmail.com>
2024-09-13 11:27:22 +0900

    testttt

--- src/both/main.jsx
+++ src/both/main.jsx
@@ -5,11 +5,11 @@ import style from './main.module.css'
 const main = () => {
   return (
   <div className={style.mainbg}>
-  <header>
   <nav>
-    <Link to='/quizone' className={style.fadein}><button>버튼</button></Link>
+    <Link to='/quizone'>
+    <img src="https://lh3.google.com/u/0/d/1RhG4ktUGAQxRiPcJQeDX16FWTRDmv-uz=w2880-h1376-iv1" alt="flower" className={style.flower}></img>
+    </Link>
   </nav>
-</header>
 </div>
   );
 };
